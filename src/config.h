1ab6a8874ea33e4e41c1be6f484279635705ad75
Poul-Henning Kamp <phk@FreeBSD.org>
2019-01-10 09:56:52 +0000

    Try to make compile on both Linux and FreeBSD seamless

--- src/config.h
+++ src/config.h
@@ -1,6 +1,12 @@
 
 #define HAVE_CLOCK_GETTIME 1
 #define HAVE_NANOSLEEP 1
-#define HAVE_STATVFS_H 1
-#define HAVE_SYS_MOUNT_H 1
 
+#ifdef __linux__
+#  define _GNU_SOURCE 1
+#  define HAVE_SYS_VFS_H 1
+#  define HAVE_FALLOCATE 1
+#else
+#  define HAVE_STATVFS_H 1
+#  define HAVE_SYS_MOUNT_H 1
+#endif
